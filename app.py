1cafe735c12e856f175276bcd2463c91b6295237
gumba21 <62870166+gumba21@users.noreply.github.com>
2026-07-23 15:36:44 -0500

    Install osu!mania import endpoints

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import backend as _backend
 import rhythm_input_lab_core as _core
 from asset_backend import install_note_atlas_endpoint
 from media_backend import install_song_media_endpoint
+from osu_import_backend import install_osu_import_endpoint
 from practice_attempt_backend import install_practice_attempt_endpoint
 from reports_backend import install_report_endpoint
 from ril_package_backend import install_ril_package_endpoint
@@ -21,6 +22,7 @@ install_note_atlas_endpoint(_backend)
 install_song_media_endpoint(_backend)
 install_practice_attempt_endpoint(_backend)
 install_ril_package_endpoint(_backend)
+install_osu_import_endpoint(_backend)
 
 RhythmApp = _backend.RhythmApp
 RecorderState = _backend.RecorderState
