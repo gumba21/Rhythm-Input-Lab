3ae44642112d81b29a0d44ce427488e99a752989
gumba21 <62870166+gumba21@users.noreply.github.com>
2026-07-23 15:38:00 -0500

    Run osu!mania importer tests locally

--- run-tests.bat
+++ run-tests.bat
@@ -8,6 +8,9 @@ if errorlevel 1 goto :failed
 py ril_package_self_test.py
 if errorlevel 1 goto :failed
 
+py osu_importer_self_test.py
+if errorlevel 1 goto :failed
+
 echo.
 echo All Rhythm Input Lab tests passed.
 pause
